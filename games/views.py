d223622d65152344c8cfce1227e677bb1e62ca11
Keiko Oda <keiko713@gmail.com>
2012-02-03 13:00:59 -0800

    modify graphical bugs, add function for join the game

--- games/views.py
+++ games/views.py
@@ -1,23 +1,50 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response, get_object_or_404
+from django.core.urlresolvers import reverse
 from django.template import RequestContext
 from games.models import *
 
-def index(request):
+def get_ongoing_games():
     # the game that finish_date is None
     ongoing_games = Game.objects.filter(finish_date=None)
     gameplayers_list = []
     for game in ongoing_games:
         gameplayers_list.append(GamePlayer.objects.filter(game=game))
-    return render_to_response('index.html', {'gameplayers_list': gameplayers_list})
+    return gameplayers_list
 
 
-def new_game(request):
-    # get ongoing games
+def get_ongoing_games_num():
     ongoing_games = Game.objects.filter(finish_date=None)
     game_number = 0
     if ongoing_games:
         game_number = len(ongoing_games)
+    return game_number
+
+
+def index(request):
+    error_message = ""
+    if request.method == 'POST':
+        game_id = request.POST.get('game_id', False)
+        if game_id:
+            name = request.POST.get('player_name' + game_id, False)
+            if name:
+                player = Player(name=name)
+                player.save()
+                game = get_object_or_404(Game, pk=game_id)
+                game_player = GamePlayer(game=game, player=player)
+                game_player.save()
+                return HttpResponseRedirect(reverse('games.views.open_game', args=(game_player.id,)))
+        error_message = "Name is required to join the game!"
+    gameplayers_list = get_ongoing_games()
+    return render_to_response('index.html', {
+        'gameplayers_list': gameplayers_list,
+        'error_message': error_message,
+    }, context_instance=RequestContext(request))
+
+
+def new_game(request):
+    # get ongoing games
+    game_number = get_ongoing_games_num()
     return render_to_response('newgame/index.html', {
         'game_number': game_number,
     }, context_instance=RequestContext(request))
@@ -26,11 +53,7 @@ def new_game(request):
 def create_game(request):
     name = request.POST.get('player_name', False)
     if not name:
-        # get ongoing games TODO: duplicated codes, it need to be fixed.
-        ongoing_games = Game.objects.filter(finish_date=None)
-        game_number = 0
-        if ongoing_games:
-            game_number = len(ongoing_games)
+        game_number = get_ongoing_games_num()
         return render_to_response('newgame/index.html', {
                 'game_number': game_number,
                 'error_message': "Name is required!",
@@ -67,3 +90,5 @@ def open_game(request, game_player_id):
         'history': history,
         'player_list': player_list,
     })
+
+
